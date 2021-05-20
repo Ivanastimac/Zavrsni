c53ca49e44ea1fbb14082b96c0e6a4fa22e69221
Ivanastimac <ivana.stimac008@gmail.com>
2021-05-20 10:43:02 +0200

    Promijenjen izgled za tasks-index

--- resources/views/tasks/tasks-index.blade.php
+++ resources/views/tasks/tasks-index.blade.php
@@ -7,17 +7,23 @@
     <table class="table">
         <thead>
             <tr>
-                <th scope="col">#</th>
                 <th scope="col">Naslov</th>
-                <th scope="col">Razina</th>
+                <th scope="col">Tekst zadatka</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($tasks as $task)
                 <tr>
-                <th scope="col"> {{ $task->id }} </th>
                 <th scope="col"> {{ $task->title }} </th>
-                <th scope="col"> {{ $task->level }} </th>
+                <th scope="col">
+                @if($task->bodyText != NULL)
+                    <span class="d-inline-block text-truncate" style="max-width: 200px;">
+                        {{ $task->bodyText }}
+                    </span>
+                @else 
+                    Priložena je slika teksta zadatka
+                @endif
+                </th>
                 <th class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <a href = "/tasks/edit/{{ $task->id }}"><button class="btn btn-success">Promjeni</button></a>
                     <a href = "/tasks/show/{{ $task->id }}"><button class="btn btn-success">Detalji</button></a>
