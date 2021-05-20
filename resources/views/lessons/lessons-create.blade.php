b0bd7abcb7bb5426672691cab847d3b83dea5df2
Ivanastimac <ivana.stimac008@gmail.com>
2021-05-20 11:50:26 +0200

    Dodana css datoteka za lessons-create.

--- resources/views/lessons/lessons-create.blade.php
+++ resources/views/lessons/lessons-create.blade.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="{{ asset('css/lessons-create.css') }}">
+
+    <!-- Include the Livewire styles -->
+    @livewireStyles
+</head>
+<body>
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -8,4 +17,6 @@
 @livewire('lessons-create')
 
 </x-app-layout>
+</body>
+</html>
 
--- resources/views/livewire/lessons-create.blade.php
+++ resources/views/livewire/lessons-create.blade.php
@@ -4,7 +4,7 @@
             <div class="col">
                 <label for="name" class = "fs-6 text-secondary p-1">Naziv cjeline</label>
                 <input type="text" wire:model="titleLesson" id="name" class = "shadow p-1 mb-4 rounded border-secondary">
-                @error('titleLesson') <span ...>{{ $message }}</span> @enderror
+                @error('titleLesson') <span class="errorMessage">{{ $message }}</span> @enderror
             </div>
             <div>
                 <button  type="submit" class="btn btn-primary btn-sm" onclick = "return confirm('Jeste li sigurni da želite spremiti ovu cjelinu? Naknadne izmjene nisu moguće!')">Spremi cjelinu</button>
@@ -12,5 +12,6 @@
         </form>
     </div>
 </div>
+
   
 
