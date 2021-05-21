06beb1a9fdf1c8dfbc60c555d9aebc7e778b18bf
lergahana <46451875+lergahana@users.noreply.github.com>
2021-05-21 13:12:34 +0200

    Uvedena mogućnost brisanja korisnika

--- app/Actions/Jetstream/DeleteUser.php
+++ app/Actions/Jetstream/DeleteUser.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Actions\Jetstream;
+use App\Models\UserTasks;
+use App\Models\UserAnsweredTasks;
 
 use Laravel\Jetstream\Contracts\DeletesUsers;
 
@@ -14,6 +16,17 @@ class DeleteUser implements DeletesUsers
      */
     public function delete($user)
     {
+        $user_task = UserTasks::where('id_user', $user->id);
+        $user_answered_task = UserAnsweredTasks::where('id_user', $user->id);
+
+        if($user_answered_task != null){
+            $user_answered_task->delete();
+        }
+
+        if($user_task != null){
+            $user_task->delete();
+        }
+
         $user->deleteProfilePhoto();
         $user->tokens->each->delete();
         $user->delete();
