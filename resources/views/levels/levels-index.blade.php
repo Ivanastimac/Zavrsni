c3c7e70e199612fa5b9639ff01675f7a681a7bac
Ivanastimac <ivana.stimac008@gmail.com>
2021-05-22 20:11:12 +0200

    Dodan dizajn za prikaz cjelina i razina

--- resources/views/levels/levels-index.blade.php
+++ resources/views/levels/levels-index.blade.php
@@ -9,14 +9,14 @@
             @foreach ($levels as $level)
                 <tr>
                 <th class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <a href = "/tasks/index/{{ $level->id }}"><button class="btn btn-success"> {{ $level->title }} </button></a>
+                    <a href = "/tasks/index/{{ $level->id }}"><button class="btn btn-primary btn-sm"> {{ $level->title }} </button></a>
                 </th>
                 </tr>
             @endforeach    
         </tbody>
     </table>
     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-        <x-jet-nav-link href="{{ route('levels/create') }}" :active="request()->routeIs('levels/create')">
+        <x-jet-nav-link class="hover:border-blue-500" href="{{ route('levels/create') }}" :active="request()->routeIs('levels/create')">
             {{ __('Dodaj razinu') }}
         </x-jet-nav-link>
     </div>
