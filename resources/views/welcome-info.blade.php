0bc06bef42143a79c464c655f4433f26aacc3f3a
Ivanastimac <ivana.stimac008@gmail.com>
2021-05-25 18:47:27 +0200

    Promijenjen welcome screen

---
+++ resources/views/welcome-info.blade.php
@@ -0,0 +1,14 @@
+
+    <div class="p-5">
+        <div class="shadow overflow-hidden sm:rounded-md">
+            <div class="px-4 py-5 bg-white sm:p-6">
+                <div class="pb-3">
+                    <span class = "border-bottom border-primary fs-5 fw-bold">
+                        Dobrodošli u web aplikaciju za e-učenje! </br>
+                    </span>
+                    Ako ste profesor, imate mogućnost stvaranja cjelina, razina i upisivanja zadataka koji bi se prikazivali studentima u igrici! </br>
+                    Ako ste student, imate pristup igrici preko koje, svojim napretkom, možete usvajati znanja vezana iz kolegija koji pohađate!
+                </div>
+            </div> 
+        </div> 
+    </div> 
